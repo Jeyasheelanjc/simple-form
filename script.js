108c13ef2c15704dffb016de2d0e6c68a8681496
JC <113685133+Jeyasheelanjc@users.noreply.github.com>
2023-11-28 16:30:19 +0530

    Update script.js

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const email = document.getElementById('email')
 const pass1 = document.getElementById('pass1')
 const pass2 = document.getElementById('pass2')
 const form = document.getElementById('form')
-
+var a = 0;
 form.addEventListener('submit', e => {
     e.preventDefault();
     checkInputs();
+    if (a == 4) {
+        isFormValid()
+    }
+    else {
+        a = 0}
 })
 const checkInputs = () => {
     userName = input.value.trim();
@@ -32,12 +37,12 @@ const checkInputs = () => {
     }
     //password1
 
-    var passlen1=passedValue.length<8
-    var passlen2=passedValue.length<8
+    var passlen1 = passedValue.length < 8
+    var passlen2 = passedValue.length < 8
     if (passedValue === '') {
         setError(pass1, 'please enter the password')
     }
-    else if(passlen1){
+    else if (passlen1) {
         setError(pass1, 'please enter the password atleast 8 digits')
     }
     else {
@@ -47,7 +52,7 @@ const checkInputs = () => {
     if (passed2Value === "") {
         setError(pass2, 'please enter the password')
     }
-    else if(passlen2){
+    else if (passlen2) {
         setError(pass2, 'please enter the password atleast 8 digits')
     }
     else if (passlen1 !== passlen2) {
@@ -55,11 +60,12 @@ const checkInputs = () => {
     }
     else {
         setSuccess(pass2)
-       
+
 
     }
 }
 const setError = (input, message) => {
+   
     const formControl = input.parentElement;
     const small = formControl.querySelector('small')
     formControl.className = 'formcontrol wrong'
@@ -67,6 +73,7 @@ const setError = (input, message) => {
 
 }
 const setSuccess = (input) => {
+    a += 1;
     const formControl = input.parentElement;
     formControl.className = 'formcontrol success'
 }
@@ -74,4 +81,10 @@ const setSuccess = (input) => {
 const isEmail = (email) => {
     return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
 }
-
+isFormValid = () => {
+    document.write("<center>")
+    document.write("Enter the Name: "+input.value+"<br>")
+    document.write("Enter the Email Id: "+email.value+"<br>")
+    document.write("Password1: "+pass1.value+"<br>")
+    document.write("Password2: "+pass2.value+"<br>")
+}
