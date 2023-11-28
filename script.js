efd0fa7a3a2ed2e76ee8fcff6b26226dafab09af
JC <113685133+Jeyasheelanjc@users.noreply.github.com>
2023-11-28 12:55:39 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,73 @@
+const input = document.getElementById('input')
+const email = document.getElementById('email')
+const pass1 = document.getElementById('pass1')
+const pass2 = document.getElementById('pass2')
+const form = document.getElementById('form')
+
+form.addEventListener('submit', e => {
+    e.preventDefault();
+    checkInputs();
+})
+const checkInputs = () => {
+    userName = input.value.trim();
+    emailValue = email.value.trim();
+    passedValue = pass1.value.trim();
+    passed2Value = pass2.value.trim();
+
+    if (userName === "") {
+        setError(input, 'user cannot be blank')
+    }
+    else {
+        setSuccess(input)
+        // document.write(input.value)
+    }
+    if (emailValue === "") {
+        setError(email, 'user cannot be blank')
+    }
+    else if (!isEmail(emailValue)) {
+        setError(email, 'not a valid email')
+    }
+    else {
+        setSuccess(email)
+        // document.write(email.value)
+    }
+    //password1
+    if (passedValue === '') {
+        setError(pass1, 'please enter the password')
+    }
+    else if(passedValue.length<8){
+        setError(pass1, 'please enter the password atleast 8 digits')
+    }
+    else {
+        setSuccess(pass1)
+        // document.write(pass1.value)
+    }
+    //password2
+    if (passed2Value === "") {
+        setError(pass2, 'please enter the password')
+    }
+    else if (passedValue !== passed2Value) {
+        setError(pass2, 'Enter the same password')
+    }
+    else {
+        setSuccess(pass2)
+       
+
+    }
+}
+const setError = (input, message) => {
+    const formControl = input.parentElement;
+    const small = formControl.querySelector('small')
+    formControl.className = 'formcontrol wrong'
+    small.innerText = message
+
+}
+const setSuccess = (input) => {
+    const formControl = input.parentElement;
+    formControl.className = 'formcontrol success'
+}
+
+const isEmail = (email) => {
+    return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
